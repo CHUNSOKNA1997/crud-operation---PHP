d0ae930bd5dfceb8a727955fd467794df9a0eccf
CHUNSOKNA1997 <chun.sokna.1222@gmail.com>
2025-03-03 12:57:25 +0700

    finished view and logical for read.php

--- create.php
+++ create.php
@@ -7,7 +7,8 @@
             $sql = "INSERT INTO tbuser (username, email) VALUES ('$username', '$email')";
             $isSuccess = $connection->query($sql);
             if($isSuccess){
-                $message = "Successfully added to database";
+                header("Location: read.php");
+                exit();
             } else {
                 $message = "Error: " . $connection->error;
             }
@@ -24,24 +25,13 @@
 </head>
 <body>
     <div class="w-screen min-h-screen flex flex-col justify-center items-center space-y-7">
-        <h2 class="text-5xl font-semibold">Create User</h2>
-        <?php if(!empty($message)): ?>
-            <div class="flex items-center p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800" role="alert">
-                <svg class="shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-                </svg>
-                <span class="sr-only">Info</span>
-                <div>
-                    <span class="font-medium">
-                        <?php echo $message ?>
-                    </span>
-                </div>
-                </div>
-        <?php endif; ?>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" class="flex flex-col items-center gap-5">
-            <input required type="text" name="username" placeholder="username" class="text-xl py-4 px-8 outline outline-gray-600 rounded-full">
-            <input required type="email" name="email" placeholder="eg. example.gmail.com" class="text-xl py-4 px-8 outline outline-gray-600 rounded-full">
-            <button name="create" type="submit" class="bg-green-600 px-8 py-3 rounded-2xl text-white hover:cursor-pointer hover:bg-green-500 transition-colors duration-300">Create</button>
+        <h2 class="text-5xl font-bold text-blue-600">Create User</h2>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" class="flex flex-col gap-5">
+            <label for="username" class="text-2xl font-semibold">Username</label>
+            <input required type="text" name="username" placeholder="username" class="text-xl py-4 px-8 outline outline-gray-600 rounded-lg">
+            <label for="email" class="text-2xl font-semibold">Email</label>
+            <input required type="email" name="email" placeholder="eg. example.gmail.com" class="text-xl py-4 px-8 outline outline-gray-600 rounded-lg">
+            <button type="submit" name="create" class="hover:cursor-pointer focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-4 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 transition-colors duration-300">Create User</button>
         </form>
     </div>
 </body>
--- read.php
+++ read.php
@@ -1,18 +1,69 @@
 <?php
     include "./database.php";
-
     $sql = "SELECT * FROM tbuser";
-    $connection->query($sql);
+    $result = $connection->query($sql);
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
     <title>READ USER</title>
 </head>
 <body>
-    
+    <main class="flex flex-col justify-center items-center mt-10 lg:mx-0 mx-3">
+        <div class="w-full xl:w-1/2 flex justify-between items-center">
+            <h2 class="text-3xl font-semibold">READ USER FROM DATABASE</h2>
+            <a href="create.php" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 transition-colors duration-300">Create User</a>
+        </div>
+        <div class="overflow-x-auto shadow-md rounded-lg m-10 w-full xl:w-1/2">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            ID
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Username
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Email
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Action
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php if($result->num_rows > 0){
+                        while($row = $result->fetch_assoc()){ ?>
+                            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    <?php echo $row["id"]; ?>
+                                </th>
+                                <td class="px-6 py-4">
+                                    <?php echo $row["username"]; ?>
+                                </td>
+                                <td class="px-6 py-4">
+                                    <?php echo $row["email"]; ?>
+                                </td>
+                                <td class="py-4 flex flex-col xl:flex-row">
+                                    <a href="edit.php?id=<?php echo $row["id"]; ?>" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 transition-colors duration-300">Edit</a>
+                                    <a href="delete.php?id=<?php echo $row["id"]; ?>" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 transition-colors duration-300">Delete</a>
+                                </td>
+                            </tr>
+                        <?php }
+                    } else { ?>
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+                            <td colspan="4" class="px-6 py-4 text-center">
+                                No users found in the database.
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </main>
 </body>
 </html>
