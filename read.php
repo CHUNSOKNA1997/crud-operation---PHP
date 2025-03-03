8ab9ba89f5f47fa0c1776002b88efbde40a74539
CHUNSOKNA1997 <chun.sokna.1222@gmail.com>
2025-03-03 15:21:41 +0700

    adjust some issue in create.php and read.php

--- read.php
+++ read.php
@@ -15,11 +15,11 @@
     <main class="flex flex-col justify-center items-center mt-10 lg:mx-0 mx-3">
         <div class="w-full xl:w-1/2 flex justify-between items-center">
             <h2 class="text-3xl font-semibold">READ USER FROM DATABASE</h2>
-            <a href="create.php" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 transition-colors duration-300">Create User</a>
+            <a href="create.php" class="focus:outline-none text-white bg-indigo-600 hover:bg-indigo-500 px-6 py-2.5 transition-colors duration-300 rounded-xl">Add User</a>
         </div>
-        <div class="overflow-x-auto shadow-md rounded-lg m-10 w-full xl:w-1/2">
-            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <div class="overflow-x-auto m-10 w-full xl:w-1/2">
+            <table class="w-full text-sm text-gray-600 text-left">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50">
                     <tr>
                         <th scope="col" class="px-6 py-3">
                             ID
@@ -38,8 +38,8 @@
                 <tbody>
                     <?php if($result->num_rows > 0){
                         while($row = $result->fetch_assoc()){ ?>
-                            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <tr class="bg-white border-b border-gray-700 border-gray-200">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                                     <?php echo $row["id"]; ?>
                                 </th>
                                 <td class="px-6 py-4">
@@ -48,14 +48,14 @@
                                 <td class="px-6 py-4">
                                     <?php echo $row["email"]; ?>
                                 </td>
-                                <td class="py-4 flex flex-col xl:flex-row">
-                                    <a href="edit.php?id=<?php echo $row["id"]; ?>" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 transition-colors duration-300">Edit</a>
-                                    <a href="delete.php?id=<?php echo $row["id"]; ?>" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 transition-colors duration-300">Delete</a>
+                                <td class="ml-0 xl:ml-5 space-y-2 xl:space-y-0 py-4 flex flex-col xl:flex-row space-x-4">
+                                    <a href="edit.php?id=<?php echo $row["id"]; ?>" class="text-center text-indigo-600 hover:text-indigo-500 hover:underline transition-colors duration-300">Edit</a>
+                                    <a href="delete.php?id=<?php echo $row["id"]; ?>" class="text-center text-red-600 hover:text-red-500 hover:underline transition-colors duration-300">Delete</a>
                                 </td>
                             </tr>
                         <?php }
                     } else { ?>
-                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+                        <tr class="bg-white border-gray-200">
                             <td colspan="4" class="px-6 py-4 text-center">
                                 No users found in the database.
                             </td>
